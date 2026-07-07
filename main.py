14d50c98e1e31c707f9cf82eae9af1f7ecf56b63
熊珂 <xk2015040317@outlook.com>
2026-07-07 03:11:33 +0000

    Author:    熊珂 <xk2015040317@outlook.com> 	modified:   main.py 	git/

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 def main():
-    pass
+    _3d_list = [[[0]]]
+    for i in range(0,100,1):
+        for j in range(0,100,1):
+            for k in range(0,100,1):
+                pass
 if __name__ == "__main__":
     main()
