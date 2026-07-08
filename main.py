709fbf7f20a8b9a1b1d49df0f861202ab31c0ab8
熊珂 <xk2015040317@outlook.com>
2026-07-08 02:58:27 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def main():
-    _3d_list = [[[0]]]
-    for i in range(0,100,1):
-        for j in range(0,100,1):
-            for k in range(0,100,1):
+    _3d_list = []
+    for i in range(100):
+        _3d_list.append([])
+        for j in range(100):
+            _3d_list[i].append([])
+            for k in range(100):
                 _3d_list[i][j].append(0)
-def __test__3d_list():
     assert len(_3d_list) == 100
     assert len(_3d_list[0]) == 100
     assert len(_3d_list[0][0]) == 100
 if __name__ == "__main__":
     main()
-    __test__3d_list()
+    
