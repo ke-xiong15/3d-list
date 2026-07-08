7afeb1ab13ae6f09e1a6e2e367b74d3e5ef7ea0f
熊珂 <xk2015040317@outlook.com>
2026-07-08 00:59:36 +0000

    modified:   main.py 	git/

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ def main():
         for j in range(0,100,1):
             for k in range(0,100,1):
                 _3d_list[i][j].append(0)
+def __test__3d_list():
+    assert len(_3d_list) == 100
+    assert len(_3d_list[0]) == 100
+    assert len(_3d_list[0][0]) == 100
 if __name__ == "__main__":
     main()
+    __test__3d_list()
