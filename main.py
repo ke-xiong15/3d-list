fa3e258e37b892b4c9a9a6ca3413b30756250e52
熊珂 <xk2015040317@outlook.com>
2026-07-09 00:56:43 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def main():
         for j in range(0,100,1):
             for k in range(0,100,1):
                 _3d_list[i][j].append(0)
+            _3d_list[i].append([0])
+        _3d_list.append([[0]])
 if __name__ == "__main__":
     main()
