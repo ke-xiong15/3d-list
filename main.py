4c2c1d242b3be8b8713e8c81c9b59f78a709496e
熊珂 <xk2015040317@outlook.com>
2026-07-11 02:34:56 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-def main():
-    _3d_list = []
-    for i in range(100):
-        _3d_list.append([])
-        for j in range(100):
-            _3d_list[i].append([])
-            for k in range(100):
-                _3d_list[i][j].append(0)
-    assert len(_3d_list) == 100
-    assert len(_3d_list[0]) == 100
-    assert len(_3d_list[0][0]) == 100
+class main:
+    def __init__(self):
+        self._3d_list = []
+    def main(self):
+        for i in range(100):
+            self._3d_list.append([])
+            for j in range(100):
+                self._3d_list[i].append([])
+                for k in range(100):
+                    self._3d_list[i][j].append(0)
+        assert len(self._3d_list) == 100
+        assert len(self._3d_list[0]) == 100
+        assert len(self._3d_list[0][0]) == 100
+
 if __name__ == "__main__":
     main()
     
